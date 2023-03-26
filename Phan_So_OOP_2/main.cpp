5199da57e2e36cc75f46064725102453ff0b4f8e
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 13:28:02 +0700

    Update main.cpp

--- Phan_So_OOP_2/main.cpp
+++ Phan_So_OOP_2/main.cpp
@@ -1,1 +1,15 @@
+#include "PhanSo.h"
 
+int main()
+{
+	PhanSo ps1;
+	ps1.nhap();
+	ps1.rutgon();
+	ps1.xuat();
+
+	double kq = ps1.TinhKetQuaPhanSo();
+	if (kq < 0) cout << "\nps1 < 0";
+	else if (kq > 0) cout << "\nps1 > 0";
+	else cout << "\nps1 = 0";
+	return 0;
+}
