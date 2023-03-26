d7fdd12c470b7c1e62dd901f3911617898757e25
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 14:55:49 +0700

    Create PhanSo.h

---
+++ Phan_So_OOP_3/PhanSo.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class PhanSo
+{
+private:
+	int tuso, mauso;
+public:
+	void Nhap();
+	void Xuat();
+	double TinhKetQuaPhanSo();
+	void RutGon();
+	PhanSo TinhTong(PhanSo);
+	PhanSo TinhHieu(PhanSo);
+	PhanSo TinhTich(PhanSo);
+	PhanSo TinhThuong(PhanSo);
+	bool KiemTraHaiPhanSo(PhanSo);
+};
+
