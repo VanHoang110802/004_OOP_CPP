338322e5c2ec866cb943e10b754105317af61404
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 15:23:21 +0700

    Update main.cpp

--- HSSV_OOP_2/main.cpp
+++ HSSV_OOP_2/main.cpp
@@ -1,1 +1,4 @@
-
+/*
+Viết chương trình nhập dữ liệu danh sách các sinh viên có trong 1 lớp biết thông tin 1 sinh viên gồm có: Mã số, họ tên, lớp, ngày sinh, số CMND, điểm thi môn chuyên ngành. Hãy tìm ra sinh viên có điểm thi chuyên ngành cao nhất lớp, 
+nếu có nhiều sinh viên có điểm thi chuyên ngành bằng nhau thì xuất ra tất cả các sinh viên đó theo thứ tự tăng dần của tên.
+*/
