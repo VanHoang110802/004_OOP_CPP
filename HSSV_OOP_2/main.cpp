73505e0b060a793f15815494b9741a8ceb679f8e
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 16:18:32 +0700

    Update main.cpp

--- HSSV_OOP_2/main.cpp
+++ HSSV_OOP_2/main.cpp
@@ -3,3 +3,54 @@ Viết chương trình nhập dữ liệu danh sách các sinh viên có trong 1
 Hãy tìm ra sinh viên có điểm thi chuyên ngành cao nhất lớp, nếu có nhiều sinh viên có điểm thi chuyên ngành bằng nhau thì xuất ra tất cả các sinh viên đó theo thứ tự tăng
 dần của tên.
 */
+
+#include "LopHoc.h"
+
+int main()
+{
+	int soLuong;
+	do
+	{
+		cout << "Nhap so luong sinh vien: ";
+		cin >> soLuong;
+		if (soLuong < 1)
+		{
+			cout << "\nSo luong nhap khong hop le!\n\n";
+		}
+	} while (soLuong < 1);
+
+	SinhVien *arrSinhVien = new SinhVien[soLuong];
+
+	NhapDanhSachLop(arrSinhVien, soLuong);
+	XuatDanhSachLop(arrSinhVien, soLuong);
+
+	cout << "\n\n";
+	vector <SinhVien> vecSinhVien;
+	double diemLonNhat = TimDiemThiCaoNhat(arrSinhVien, soLuong);
+
+	for (int i = 0; i < soLuong; i++)
+	{
+		if (arrSinhVien[i].LayDiemThi() == diemLonNhat)
+		{
+			vecSinhVien.push_back(arrSinhVien[i]);
+		}
+	}
+
+	if (vecSinhVien.size() == 1)
+	{
+		vecSinhVien[0].Xuat();
+	}
+	else
+	{
+		SapTangDanTheoTen(vecSinhVien);
+
+		// Xuất ra.
+		for (int i = 0; i < vecSinhVien.size(); i++)
+		{
+			vecSinhVien[i].Xuat();
+		}
+	}
+
+	delete[]arrSinhVien;
+	return 0;
+}
