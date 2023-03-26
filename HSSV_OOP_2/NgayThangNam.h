10cde0272aafb6706f05dd84b92063311de4234f
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 16:17:08 +0700

    Create NgayThangNam.h

---
+++ HSSV_OOP_2/NgayThangNam.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+class NgayThangNam
+{
+private:
+	int ngay, thang, nam;
+public:
+	void NhapNgayThangNam();
+	void XuatNgayThangNam();
+};
