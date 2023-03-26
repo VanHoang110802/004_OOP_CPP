9830f09f9cfb28f196a01c1c0df0ee67916079f6
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 14:26:30 +0700

    Update PhanSo.cpp

--- Phan_So_OOP_2/PhanSo.cpp
+++ Phan_So_OOP_2/PhanSo.cpp
@@ -1,38 +1,20 @@
-#include "PhanSo.h"
+#pragma once
 
-void PhanSo::nhap()
-{
-	cout << "Nhap tu so: "; cin >> tuso;
-	do
-	{
-		cout << "Nhap mau so: "; cin >> mauso;
-		if (mauso == 0) cout << "\nMau so nhap khong hop le!\n";
-	} while (mauso == 0);
-}
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-void PhanSo::xuat()
+class PhanSo
 {
-	if (mauso < 0) mauso *= -1, tuso *= -1;
-	cout << '\n' << tuso << '/' << mauso;
-}
-
-double PhanSo::TinhKetQuaPhanSo()
-{
-	return (double)tuso / mauso;
-}
-
-int __gcd(int a, int b)
-{
-	if (a < 0) a *= -1;
-	if (b < 0) b *= -1;
-	int MIN = min(a, b), MAX = max(a, b);
-	if (MAX % MIN == 0) return MIN;
-	for (int i = MIN / 2; i >= 1; --i) if (MIN % i == 0 && MAX % i == 0) return i;
-}
-
-void PhanSo::rutgon()
-{
-	int temp = __gcd(tuso, mauso);
-	tuso /= temp;
-	mauso /= temp;
-}
+private:
+	int tuso, mauso;
+public:
+	void Nhap();
+	void Xuat();
+	double TinhKetQuaPhanSo();
+	void RutGon();
+	PhanSo TinhTong(PhanSo);
+	PhanSo TinhHieu(PhanSo);
+	PhanSo TinhTich(PhanSo);
+	PhanSo TinhThuong(PhanSo);
+};
