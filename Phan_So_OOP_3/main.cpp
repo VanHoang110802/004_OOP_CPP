cbb01201fb14b814f8bb6150ccbb0716d98f75ca
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-26 14:56:22 +0700

    Update main.cpp

--- Phan_So_OOP_3/main.cpp
+++ Phan_So_OOP_3/main.cpp
@@ -1,1 +1,39 @@
 
+#include "PhanSo.h"
+
+int main()
+{
+	PhanSo ps1, ps2;
+	ps1.Nhap();
+	ps1.RutGon();
+	ps2.Nhap();
+	ps2.RutGon();
+	ps1.Xuat();
+	cout << '\n';
+	ps2.Xuat();
+	cout << "\nps1 + ps2 = ";
+	PhanSo Tong = ps1.TinhTong(ps2);
+	Tong.RutGon();
+	Tong.Xuat();
+
+	cout << "\nps1 - ps2 = ";
+	PhanSo Hieu = ps1.TinhHieu(ps2);
+	Hieu.RutGon();
+	Hieu.Xuat();
+
+	cout << "\nps1 * ps2 = ";
+	PhanSo Tich = ps1.TinhTich(ps2);
+	Tich.RutGon();
+	Tich.Xuat();
+
+	cout << "\nps1 / ps2 = ";
+	PhanSo Thuong = ps1.TinhThuong(ps2);
+	Thuong.RutGon();
+	Thuong.Xuat();
+
+	if (ps1.KiemTraHaiPhanSo(ps2))
+		cout << "\nps1 > ps2";
+	else
+		cout << "\nps1 <= ps2";
+	return 0;
+}
